287f9f8cd1a79284fa400227307e414e2e43bfba
dongsiqiao <1247143911@qq.com>
2021-01-29 14:55:17 +0800

    12_配置Authorization和获取左侧菜单数据并渲染

--- src/main.js
+++ src/main.js
@@ -7,9 +7,22 @@ import './plugins/element.js'
 import './assets/css/global.css'
 // 导入字体图标
 import './assets/fonts/iconfont.css'
+
 //导入axios接口
 import axios from 'axios'
 axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
+//请求拦截器
+axios.interceptors.request.use((config) => {
+  config.headers.Authorization = window.sessionStorage.getItem('token')
+  return config
+})
+//响应拦截器
+axios.interceptors.response.use((res) => {
+  if (res.data.meta.msg === '无效token' && res.data.meta.status === 400) {
+    location.href = '/#/login'
+  }
+  return res
+})
 Vue.prototype.$http = axios
 
 Vue.config.productionTip = false
