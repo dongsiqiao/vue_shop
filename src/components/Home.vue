a8f83ead11e914dea0a70387a70bde4b9abb34b9
dongsiqiao <1247143911@qq.com>
2021-01-29 13:52:19 +0800

    11_Home结构梳理

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -2,13 +2,39 @@
   <el-container class="home-container">
     <!-- 头部 -->
     <el-header>
-      Header
+      <div>
+        <img src="../assets/heima.png" alt />
+        <span>电商后台管理系统</span>
+      </div>
       <el-button type="info" @click="logout">退出</el-button>
     </el-header>
     <!-- 页面主体 -->
     <el-container>
       <!-- 侧边栏 -->
-      <el-aside width="200px">Aside</el-aside>
+      <el-aside width="200px">
+        <!-- 侧边栏菜单区域 -->
+        <el-menu background-color="#333744" text-color="#fff" active-text-color="#ffd04b">
+          <!-- 一级菜单 -->
+          <el-submenu index="1">
+            <!-- 一级菜单的模板区域 -->
+            <template slot="title">
+              <!-- 图标 -->
+              <i class="el-icon-location"></i>
+              <!-- 文本 -->
+              <span>导航一</span>
+            </template>
+            <!-- 二级菜单 -->
+            <el-menu-item index="1-4-1">
+              <template slot="title">
+                <!-- 图标 -->
+                <i class="el-icon-location"></i>
+                <!-- 文本 -->
+                <span>导航一</span>
+              </template>
+            </el-menu-item>
+          </el-submenu>
+        </el-menu>
+      </el-aside>
       <!-- 右侧主体 -->
       <el-main>Main</el-main>
     </el-container>
@@ -31,7 +57,20 @@ export default {
   height: 100%;
 }
 .el-header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
   background-color: #373d41;
+  padding-left: 0;
+  color: #fff;
+  font-size: 20px;
+  > div {
+    display: flex;
+    align-items: center;
+    span {
+      margin-left: 15px;
+    }
+  }
 }
 .el-aside {
   background-color: #333744;
