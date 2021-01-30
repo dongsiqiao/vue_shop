c33c509c6aa7a95f799a0c76a14d38f5b49cc238
dongsiqiao <1247143911@qq.com>
2021-01-30 14:24:54 +0800

    26_对话框关闭重置表单和添加用户

--- src/components/user/Users.vue
+++ src/components/user/Users.vue
@@ -48,7 +48,7 @@
       <el-pagination @size-change="handleSizeChange" @current-change="handleCurrentChange" :current-page="queryInfo.pagenum" :page-sizes="[1, 2, 5, 10]" :page-size="queryInfo.pagesize" layout="total, sizes, prev, pager, next, jumper" :total="total"></el-pagination>
     </el-card>
     <!-- 添加用户对话框 -->
-    <el-dialog title="提示" :visible.sync="addDialogVisible" width="40%">
+    <el-dialog title="提示" :visible.sync="addDialogVisible" width="40%" @close="addDialogClosed">
       <!-- 内容主体 -->
       <el-form :model="addForm" :rules="addFormRules" ref="addFormRef" label-width="70px">
         <el-form-item label="用户名" prop="username">
@@ -67,7 +67,7 @@
       <!-- 底部区域 -->
       <span slot="footer" class="dialog-footer">
         <el-button @click="addDialogVisible = false">取 消</el-button>
-        <el-button type="primary" @click="addDialogVisible = false">确 定</el-button>
+        <el-button type="primary" @click="addUser">确 定</el-button>
       </span>
     </el-dialog>
   </div>
@@ -172,6 +172,24 @@ export default {
         return this.$message.error('更新用户失败')
       }
       this.$message.success('更新用户成功')
+    },
+    // 监听添加用户对话框的关闭事件
+    addDialogClosed() {
+      this.$refs.addFormRef.resetFields()
+    },
+    addUser() {
+      this.$refs.addFormRef.validate(async (valid) => {
+        if (!valid) return false
+        // 可以发起添加用户的网络请求
+        const { data: res } = await this.$http.post('users', this.addForm)
+        if (res.meta.status !== 201) {
+          return this.$message.error('添加用户失败')
+        }
+        this.$message.success('添加用户成功')
+        this.addDialogVisible = false
+        // 刷新用户列表
+        this.getUserList()
+      })
     }
   }
 }
