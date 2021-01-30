a848dde80809c9a003ab22084211a62a7979ce64
dongsiqiao <1247143911@qq.com>
2021-01-30 10:53:25 +0800

    22_修改用户列表状态功能

--- src/components/user/Users.vue
+++ src/components/user/Users.vue
@@ -28,7 +28,7 @@
         <el-table-column label="角色" prop="role_name"></el-table-column>
         <el-table-column label="状态">
           <template slot-scope="scope">
-            <el-switch v-model="scope.row.mg_state"></el-switch>
+            <el-switch v-model="scope.row.mg_state" @change="userStateChanged(scope.row)"></el-switch>
           </template>
         </el-table-column>
         <el-table-column label="操作" width="180px">
@@ -84,6 +84,15 @@ export default {
     handleCurrentChange(newPage) {
       this.queryInfo.pagenum = newPage
       this.getUserList()
+    },
+    // 监听 switch 开关状态的变化
+    async userStateChanged(userinfo) {
+      const { data: res } = await this.$http.put(`users/${userinfo.id}/state/${userinfo.mg_state}`)
+      if (res.meta.status !== 200) {
+        userinfo.mg_state = !userinfo.mg_state
+        return this.$message.error('更新用户失败')
+      }
+      this.$message.success('更新用户成功')
     }
   }
 }
