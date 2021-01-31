4cf5493b459bcf11913c0f1716a7b068bf619259
dongsiqiao <1247143911@qq.com>
2021-01-31 16:36:58 +0800

    42_初步使用vue-table-with-tree-grid

--- src/components/goods/Cate.vue
+++ src/components/goods/Cate.vue
@@ -14,6 +14,7 @@
         </el-col>
       </el-row>
       <!-- 表格区域 -->
+      <tree-table :expand-type="false" :data="catelist" :columns="columns" :selection-type="false" show-index index-text="#" border :show-row-hover="false"></tree-table>
       <!-- 分页区域 -->
     </el-card>
   </div>
@@ -32,7 +33,14 @@ export default {
       // 商品分类的数据列表，默认为空
       catelist: [],
       // 总数据条数
-      total: 0
+      total: 0,
+      // 为 table 指定列的定义
+      columns: [
+        {
+          label: '分类名称',
+          prop: 'cat_name'
+        }
+      ]
     }
   },
   created() {
@@ -51,7 +59,7 @@ export default {
       this.catelist = res.data.result
       // 为总数据条数赋值
       this.total = res.data.total
-      console.log(this.catelist)
+      // console.log(this.catelist)
     }
   }
 }
