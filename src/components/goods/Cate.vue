7a697dad5109a7fe35f2aa412fd4c2480a363c4d
dongsiqiao <1247143911@qq.com>
2021-02-02 09:59:15 +0800

    47_添加分类同步数据到后台

--- src/components/goods/Cate.vue
+++ src/components/goods/Cate.vue
@@ -14,23 +14,10 @@
         </el-col>
       </el-row>
       <!-- 表格区域 -->
-      <tree-table
-        :data="catelist"
-        :columns="columns"
-        :selection-type="false"
-        show-index
-        index-text="#"
-        border
-        :show-row-hover="false"
-        class="treeTable"
-      >
+      <tree-table :data="catelist" :columns="columns" :selection-type="false" show-index index-text="#" border :show-row-hover="false" class="treeTable">
         <!-- 是否有效 -->
         <template slot="isok" slot-scope="scope">
-          <i
-            class="el-icon-success"
-            v-if="scope.row.cat_deleted === false"
-            style="color: lightgreen;"
-          ></i>
+          <i class="el-icon-success" v-if="scope.row.cat_deleted === false" style="color: lightgreen;"></i>
           <i class="el-icon-error" v-else style="color: red;"></i>
         </template>
         <!-- 排序 -->
@@ -46,25 +33,12 @@
         </template>
       </tree-table>
       <!-- 分页区域 -->
-      <el-pagination
-        @size-change="handleSizeChange"
-        @current-change="handleCurrentChange"
-        :current-page="queryInfo.pagenum"
-        :page-sizes="[3, 5, 10, 15]"
-        :page-size="queryInfo.pagesize"
-        layout="total, sizes, prev, pager, next, jumper"
-        :total="total"
-      ></el-pagination>
+      <el-pagination @size-change="handleSizeChange" @current-change="handleCurrentChange" :current-page="queryInfo.pagenum" :page-sizes="[3, 5, 10, 15]" :page-size="queryInfo.pagesize" layout="total, sizes, prev, pager, next, jumper" :total="total"></el-pagination>
     </el-card>
     <!-- 添加分类的对话框 -->
     <el-dialog title="添加分类" :visible.sync="addCateDialogVisible" width="50%" @close="addCateDialogClosed">
       <!-- 添加分类表单 -->
-      <el-form
-        :model="addCateForm"
-        :rules="addCateFormRules"
-        ref="addCateFormRef"
-        label-width="100px"
-      >
+      <el-form :model="addCateForm" :rules="addCateFormRules" ref="addCateFormRef" label-width="100px">
         <el-form-item label="分类名称：" prop="cat_name">
           <el-input v-model="addCateForm.cat_name"></el-input>
         </el-form-item>
@@ -72,13 +46,7 @@
           <!-- options: 指定数据源 -->
           <!-- props: 用来指定配置对象 -->
           <!-- change-on-select: 允许选择一级分类 -->
-          <el-cascader
-            :options="parentCateList"
-            :props="cascaderProps"
-            v-model="selectedKeys"
-            @change="parentCateChanged"
-            clearable
-          ></el-cascader>
+          <el-cascader :options="parentCateList" :props="cascaderProps" v-model="selectedKeys" @change="parentCateChanged" clearable></el-cascader>
         </el-form-item>
       </el-form>
       <span slot="footer" class="dialog-footer">
@@ -139,9 +107,7 @@ export default {
         cat_level: 0
       },
       addCateFormRules: {
-        cat_name: [
-          { required: true, message: '请输入分类名称', trigger: 'blur' }
-        ]
+        cat_name: [{ required: true, message: '请输入分类名称', trigger: 'blur' }]
       },
       // 父级分类列表
       parentCateList: [],
@@ -207,9 +173,7 @@ export default {
       // 如果 selectedKeys 数组的 length 大于 0，证明选中了父级分类
       if (this.selectedKeys.length > 0) {
         // 父级分类的 ID
-        this.addCateForm.cat_pid = this.selectedKeys[
-          this.selectedKeys.length - 1
-        ]
+        this.addCateForm.cat_pid = this.selectedKeys[this.selectedKeys.length - 1]
         // 为当前分类的等级赋值
         this.addCateForm.cat_level = this.selectedKeys.length
       } else {
@@ -219,7 +183,17 @@ export default {
     },
     // 点击按钮添加新的分类
     addCate() {
-      console.log(this.addCateForm)
+      // console.log(this.addCateForm)
+      this.$refs.addCateFormRef.validate(async (valid) => {
+        if (!valid) return false
+        const { data: res } = await this.$http.post('categories', this.addCateForm)
+        if (res.meta.status !== 201) {
+          return this.$message.error('添加分类失败')
+        }
+        this.$message.success('添加分类成功')
+        this.getCateList()
+        this.addCateDialogVisible = false
+      })
       // this.addCateDialogVisible = false
     },
     // 监听对话框的关闭事件，重置表单数据
