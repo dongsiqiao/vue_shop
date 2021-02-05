92ca169b3db13c294277d3480c882f79aa49dc6e
dongsiqiao <1247143911@qq.com>
2021-02-05 11:11:55 +0800

    62_富文本.预校验.处理goods_cat和attrs并添加商品

--- src/main.js
+++ src/main.js
@@ -10,6 +10,12 @@ import './assets/fonts/iconfont.css'
 
 import TreeTable from 'vue-table-with-tree-grid'
 
+import VueQuillEditor from 'vue-quill-editor'
+
+import 'quill/dist/quill.core.css' // import styles
+import 'quill/dist/quill.snow.css' // for snow theme
+import 'quill/dist/quill.bubble.css' // for bubble theme
+
 //导入axios接口
 import axios from 'axios'
 axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
@@ -30,6 +36,8 @@ Vue.prototype.$http = axios
 Vue.config.productionTip = false
 
 Vue.component('tree-table', TreeTable)
+// 将富文本编辑器注册为全局可用的组件
+Vue.use(VueQuillEditor)
 
 Vue.filter('dateFormat', function(originVal) {
   const dt = new Date(originVal)
